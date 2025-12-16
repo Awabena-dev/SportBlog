916d01ddbbe74101ac7ed38a66d935ff8164f77d
awab <awabena.dev@gmail.com>
2025-12-16 06:53:49 -0800

    frist upload and make navabr

---
+++ src/section/Navbar/MobileNav.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/lib/utils'
+import { X } from 'lucide-react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+
+type navLinks = {
+    id: number;
+    path: string;
+    name: string;
+}
+
+interface mobileNavProp {
+    isOpen: boolean
+    toggle: () => void
+    navLinks: navLinks[]
+}
+
+
+const MobileNav = ({ isOpen, toggle, navLinks }: mobileNavProp) => {
+    return (
+        <>
+            <div onClick={() => toggle()} className={cn(' fixed top-0 left-0 w-full h-full bg-primary-50/60 backdrop-blur-sm z-10 transition-all duration-150', isOpen ? "translate-x-0" : "translate-x-full")}></div>
+            <div className={cn('w-[60%] h-full py-6 px-4 bg-primary-400 fixed top-0 right-0 z-50 transition-all duration-300 delay-150', isOpen ? "translate-x-0" : "translate-x-full")}>
+
+                <div>
+                    <X onClick={() => toggle()} className='size-6 mb-6' />
+                </div>
+                {/* Navlinks */}
+                <nav>
+                    <ul className='flex flex-col gap-y-2.5'>
+                        {navLinks.map((link) => {
+                            const isActive = usePathname() === link.path
+                            return <li key={link.id}
+                                className={cn(
+                                    'text-2xl text-secondray/60 font-normal hover:text-black hover:font-bold transition-all duration-300',
+                                    isActive && "text-black font-bold"
+                                )}>
+                                <Link href={link.path}>{link.name}</Link>
+                            </li>
+                        })}
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default MobileNav
---
+++ src/section/Navbar/Navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { Button } from '@/components/ui/button'
+import { Search, Menu } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import MobileNav from './MobileNav'
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+
+// NavLink
+const navLinks = [
+    {
+        id: 1,
+        path: '/',
+        name: "Home"
+    },
+    {
+        id: 2,
+        path: '/Category',
+        name: "Category"
+    },
+    {
+        id: 3,
+        path: '/Trending',
+        name: "Trending News"
+    },
+    {
+        id: 4,
+        path: '/Recent',
+        name: "Recent News"
+    },
+    {
+        id: 5,
+        path: '/Ranking',
+        name: "Ranking!"
+    },
+    {
+        id: 6,
+        path: '/Articales',
+        name: "Sport Articale"
+    },
+]
+
+
+function Navbar() {
+
+    const [isOpen, setisOpen] = useState(false);
+
+    const toggle = () => {
+        setisOpen(!isOpen);
+    };
+
+    return (
+        <header className='fixed bg-primary-50 top-0 left-0 w-full  h-23'>
+            <div className='max-wrapper flex items-center justify-between h-full'>
+
+                {/* Logo */}
+                <Image src={"/Layout/logo.svg"} width={154} height={42} alt='Logo' className='w-24 h-6.5 md:w-38.5 md:h-10.5' />
+
+                {/* Navlinks */}
+                <nav>
+                    <ul className=' hidden md:flex items-center gap-3.5'>
+                        {navLinks.map((link) => {
+
+                            const isActive = usePathname() === link.path
+                            return (
+                                <li key={link.id} className={cn(
+                                    'body-1 text-secondray/60 font-normal hover:text-black hover:font-bold transition-all duration-300',
+                                    isActive && "text-black font-bold"
+                                )}><Link href={link.path}>{link.name}</Link></li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+
+
+                {/* Serch button */}
+                <div className='flex items-center gap-4'>
+                    <Button
+                        variant={'primary'}
+                        size={'md'}
+                        className='p-2 md:py-2.5 md:px-3.5'>
+                        <Search className=' size-5 md:size-4' />
+                        <p className=' hidden md:block'>Search</p>
+
+                    </Button>
+
+                    {/* Menu */}
+                    <div onClick={() => { toggle() }} className='block md:hidden'>
+                        <Menu className='size-5' />
+                    </div>
+                </div>
+                {/* Mobile Navbar */}
+                <MobileNav isOpen={isOpen} toggle={toggle} navLinks={navLinks} />
+
+            </div>
+        </header>
+    );
+}
+
+export default Navbar
